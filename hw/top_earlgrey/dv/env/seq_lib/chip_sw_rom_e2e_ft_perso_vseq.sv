f74d7b86e39bf5874354de8bc27f59d2f70b1ef6
Harry Callahan <hcallahan@lowrisc.org>
2025-06-10 14:54:21 +0100

    Comments & style - no functional change

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_ft_perso_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_ft_perso_vseq.sv
@@ -13,18 +13,22 @@ class chip_sw_rom_e2e_ft_perso_vseq extends
     // Drive SW straps for bootstrap.
     `uvm_info(`gfn, "Driving SW straps high for bootstrap.", UVM_LOW)
     cfg.chip_vif.sw_straps_if.drive(3'h7);
+
     `uvm_info(`gfn, "Initializing SPI flash bootstrap.", UVM_LOW)
     spi_device_load_bootstrap({cfg.sw_images[SwTypeTestSlotA], ".64.vmem"});
+
     `uvm_info(`gfn, "SPI flash bootstrap done.", UVM_LOW)
 
     // Wait for SRAM initialization to complete a second time (after bootstrap).
     `uvm_info(`gfn, "Waiting for SRAM initialization to complete (after bootstrap).", UVM_LOW)
+
     `DV_WAIT(cfg.chip_vif.sram_ret_init_done == 1,
              $sformatf({"Timeout occurred when waiting for SRAM initialization; ",
                         "Current sram_ret_init_done = 1'%0b, Timeout value = %0dns"},
                         cfg.chip_vif.sram_ret_init_done,
                         cfg.sw_test_timeout_ns),
              cfg.sw_test_timeout_ns)
+
     `uvm_info(`gfn, "ROM SRAM initialization done.", UVM_LOW)
 
     // Wait for IOA1 (perso done GPIO indicator) to toggle on.
