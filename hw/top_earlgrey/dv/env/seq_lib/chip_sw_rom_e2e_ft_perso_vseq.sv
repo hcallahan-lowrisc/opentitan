17ffeb29c6a3d3862e3aeff81c133b740a0285ad
Harry Callahan <hcallahan@lowrisc.org>
2025-06-26 20:34:24 +0100

    [dv,sival] Enable use_spi_load_bootstrap for bootstrapping of ate_manuf sims
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_ft_perso_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_ft_perso_vseq.sv
@@ -10,25 +10,19 @@ class chip_sw_rom_e2e_ft_perso_vseq extends
   virtual task body();
     super.body();
 
-    // Drive SW straps for bootstrap.
-    `uvm_info(`gfn, "Driving SW straps high for bootstrap.", UVM_LOW)
-    cfg.chip_vif.sw_straps_if.drive(3'h7);
-
     `uvm_info(`gfn, "Initializing SPI flash bootstrap.", UVM_LOW)
     spi_device_load_bootstrap({cfg.sw_images[SwTypeTestSlotA], ".64.vmem"});
-
+    cfg.use_spi_load_bootstrap = 1'b0;
     `uvm_info(`gfn, "SPI flash bootstrap done.", UVM_LOW)
 
     // Wait for SRAM initialization to complete a second time (after bootstrap).
     `uvm_info(`gfn, "Waiting for SRAM initialization to complete (after bootstrap).", UVM_LOW)
-
     `DV_WAIT(cfg.chip_vif.sram_ret_init_done == 1,
              $sformatf({"Timeout occurred when waiting for SRAM initialization; ",
                         "Current sram_ret_init_done = 1'%0b, Timeout value = %0dns"},
                         cfg.chip_vif.sram_ret_init_done,
                         cfg.sw_test_timeout_ns),
              cfg.sw_test_timeout_ns)
-
     `uvm_info(`gfn, "ROM SRAM initialization done.", UVM_LOW)
 
     // Wait for IOA1 (perso done GPIO indicator) to toggle on.
