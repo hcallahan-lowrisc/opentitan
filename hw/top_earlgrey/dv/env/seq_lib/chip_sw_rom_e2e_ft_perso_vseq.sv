aa44dcafc310f860eb59c0252182b0e468008a2a
Harry Callahan <hcallahan@lowrisc.org>
2025-07-08 14:33:36 +0100

    Add rom_e2e_ft_personlize_emulation simulation

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_ft_perso_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_ft_perso_vseq.sv
@@ -10,10 +10,12 @@ class chip_sw_rom_e2e_ft_perso_vseq extends
   virtual task body();
     super.body();
 
-    `uvm_info(`gfn, "Initializing SPI flash bootstrap.", UVM_LOW)
-    spi_device_load_bootstrap({cfg.sw_images[SwTypeTestSlotA], ".64.vmem"});
-    `uvm_info(`gfn, "SPI flash bootstrap done.", UVM_LOW)
-    cfg.use_spi_load_bootstrap = 1'b0;
+    `uvm_info(`gfn, "chip_sw_rom_e2e_ft_perso_vseq::body()", UVM_LOW)
+
+    // `uvm_info(`gfn, "Initializing SPI flash bootstrap.", UVM_LOW)
+    // spi_device_load_bootstrap({cfg.sw_images[SwTypeTestSlotA], ".64.vmem"});
+    // `uvm_info(`gfn, "SPI flash bootstrap done.", UVM_LOW)
+    // cfg.use_spi_load_bootstrap = 1'b0;
 
     // Wait for SRAM initialization to complete a second time (after bootstrap).
     // `uvm_info(`gfn, "Waiting for SRAM initialization to complete (after bootstrap).", UVM_LOW)
@@ -25,10 +27,15 @@ class chip_sw_rom_e2e_ft_perso_vseq extends
     //          cfg.sw_test_timeout_ns)
     // `uvm_info(`gfn, "ROM SRAM initialization done.", UVM_LOW)
 
-    // Wait for IOA1 (perso done GPIO indicator) to toggle on.
-    `DV_WAIT(cfg.chip_vif.mios[top_earlgrey_pkg::MioPadIoa1] == '1,
-             $sformatf("Timed out waiting for IOA1 to go high."),
+    // Wait for IOA5 (SPI console TX ready signal) to toggle on.
+    `DV_WAIT(cfg.chip_vif.mios[top_earlgrey_pkg::MioPadIoa5] == '1,
+             $sformatf("Timed out waiting for IOA5 to go high."),
              cfg.sw_test_timeout_ns)
+
+    // Wait for IOA1 (perso done GPIO indicator) to toggle on.
+    // `DV_WAIT(cfg.chip_vif.mios[top_earlgrey_pkg::MioPadIoa1] == '1,
+    //          $sformatf("Timed out waiting for IOA1 to go high."),
+    //          cfg.sw_test_timeout_ns)
 
     // Set test passed.
     override_test_status_and_finish(.passed(1'b1));
