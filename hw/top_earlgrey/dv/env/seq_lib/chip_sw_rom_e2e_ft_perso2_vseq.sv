aa2934cdd9d287c8f13faa206d4de41af5f28b9c
Harry Callahan <hcallahan@lowrisc.org>
2025-08-05 17:17:14 +0100

    Rename perso2_vseq -> perso_dump_vseq

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_ft_perso2_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_ft_perso2_vseq.sv
@@ -2,14 +2,14 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class chip_sw_rom_e2e_ft_perso2_vseq extends chip_sw_rom_e2e_base_vseq;
-  `uvm_object_utils(chip_sw_rom_e2e_ft_perso2_vseq)
+class chip_sw_rom_e2e_ft_perso_dump_vseq extends chip_sw_rom_e2e_base_vseq;
+  `uvm_object_utils(chip_sw_rom_e2e_ft_perso_dump_vseq)
   `uvm_object_new
 
   virtual task body();
     super.body();
 
-    `uvm_info(`gfn, "chip_sw_rom_e2e_ft_perso2_vseq::body()", UVM_LOW)
+    `uvm_info(`gfn, "chip_sw_rom_e2e_ft_perso_dump_vseq::body()", UVM_LOW)
 
     // IOA6 (GPIO4) is for SPI console RX ready signal.
     // IOA5 (GPIO3) is for SPI console TX ready signal.
@@ -29,11 +29,7 @@ class chip_sw_rom_e2e_ft_perso2_vseq extends chip_sw_rom_e2e_base_vseq;
     // and load the bootstrapped Flash image.
 
     // Wait for IOA4 (TestStart)
-    `uvm_info(`gfn, "Waiting for IOA4 to go high now...", UVM_LOW)
-    `DV_WAIT(cfg.chip_vif.mios[top_earlgrey_pkg::MioPadIoa4] == '1,
-             $sformatf("Timed out waiting for IOA4 to go high."),
-             cfg.sw_test_timeout_ns)
-    `uvm_info(`gfn, "Saw IOA4 go high now!", UVM_LOW)
+    await_ioa("IOA4");
 
     `uvm_info(`gfn, "Dumping Flash (init-image) to disk.", UVM_LOW)
     cfg.mem_bkdr_util_h[FlashBank0Data].write_mem_to_file("dump_FlashBank0Data_init.64.scr.vmem");
@@ -42,11 +38,7 @@ class chip_sw_rom_e2e_ft_perso2_vseq extends chip_sw_rom_e2e_base_vseq;
     cfg.mem_bkdr_util_h[FlashBank1Info].write_mem_to_file("dump_FlashBank1Info_init.64.scr.vmem");
 
     // Wait for IOA5 (SPI console TX ready), for the first point the software is awaiting HOST input
-    `uvm_info(`gfn, "Waiting for IOA5 to go high now...", UVM_LOW)
-    `DV_WAIT(cfg.chip_vif.mios[top_earlgrey_pkg::MioPadIoa5] == '1,
-             $sformatf("Timed out waiting for IOA5 to go high."),
-             cfg.sw_test_timeout_ns)
-    `uvm_info(`gfn, "Saw IOA5 go high now!", UVM_LOW)
+    await_ioa("IOA5");
 
     // At this point, the test binary has already written to the CreatorSwCfg flash data region default
     // Hence, we need to reset the chip and re-bootstrap a new binary that is loaded into flash
@@ -69,11 +61,7 @@ class chip_sw_rom_e2e_ft_perso2_vseq extends chip_sw_rom_e2e_base_vseq;
     // Wait for the chip to reboot and load the transport-image to Flash.
 
     // Wait for IOA4 (TestStart)
-    `uvm_info(`gfn, "Waiting for IOA4 to go high now...", UVM_LOW)
-    `DV_WAIT(cfg.chip_vif.mios[top_earlgrey_pkg::MioPadIoa4] == '1,
-             $sformatf("Timed out waiting for IOA4 to go high."),
-             cfg.sw_test_timeout_ns)
-    `uvm_info(`gfn, "Saw IOA4 go high now!", UVM_LOW)
+    await_ioa("IOA4");
 
     // Now, the final transport image has been loaded into flash, and we can dump it again to
     // get images suitable for backdoor-loading
@@ -87,19 +75,14 @@ class chip_sw_rom_e2e_ft_perso2_vseq extends chip_sw_rom_e2e_base_vseq;
     cfg.mem_bkdr_util_h[FlashBank0Info].write_mem_to_file("dump_FlashBank0Info_transport.64.scr.vmem");
     cfg.mem_bkdr_util_h[FlashBank1Info].write_mem_to_file("dump_FlashBank1Info_transport.64.scr.vmem");
 
-    // Wait for IOA0 (ErrorReporting)
-    // (temporarily added to the block we should now be reaching which awaits SPI console input)
-    `uvm_info(`gfn, "Waiting for IOA0 to go high now...", UVM_LOW)
-    `DV_WAIT(cfg.chip_vif.mios[top_earlgrey_pkg::MioPadIoa0] == '1,
-             $sformatf("Timed out waiting for IOA0 to go high."),
-             cfg.sw_test_timeout_ns)
-    `uvm_info(`gfn, "Saw IOA0 go high now!", UVM_LOW)
-
-    // Set test passed.
-    override_test_status_and_finish(.passed(1'b1));
+    // Wait for IOA5 (SPI console TX ready)
+    // > This should now be the point where the device is waiting for SPI Console input, marked by
+    // > the following message to the console:
+    // > > "Waiting For RMA Unlock Token Hash ..."
+    await_ioa("IOA5");
 
     // Set test passed.
     override_test_status_and_finish(.passed(1'b1));
   endtask
 
-endclass : chip_sw_rom_e2e_ft_perso2_vseq
+endclass : chip_sw_rom_e2e_ft_perso_dump_vseq
