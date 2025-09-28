80474271014e44845ef3ac986fa91876bc066734
Harry Callahan <hcallahan@lowrisc.org>
2025-09-29 00:39:56 +0100

    Add test for common.blockify()

--- util/design/lib/test_common.py
+++ util/design/lib/test_common.py
@@ -52,3 +52,38 @@ class TestGetHd:
         )
 
 
+# common.blockify() test vectors
+blockify_checks = [
+    # Single line output
+    ('0xa', 16, "8'h10"),
+    ('0x12345678', 16, "36'h3_05419896"),
+    ('0x3838383838383838383', 64, "92'h1659309_94849066_74946947"),
+
+    # Input string without the '0x' decorator
+    ('a', 16, "8'h10"),
+    ('12345678', 16, "36'h3_05419896"),
+    ('3838383838383838383', 64, "92'h1659309_94849066_74946947"),
+
+    # Multi line output (i.e. wrapping due to 'limit')
+    ('0x66666666', 2, "8'h17,\n  8'h17,\n  8'h98,\n  8'h69,\n  8'h18"),
+    ('0x1234567812345678', 8,
+       "12'h131,\n  32'h17684651,\n  32'h73141112"),
+    ('0x4848595960601221233234434554', 32,
+       "8'h14,\n  128'h66065571_37933872_43946778_15166292"),
+    ('0xb5ec66efe1fb7d3393dbaf8596279a7e6150e8d6691960fc1ab0d49fc6820c114bf6560ce605d75416c3db010e962a60ab335d5d375e3021d6702049', 64,
+       "68'h2_21843458_15064031,\n  256'h84668764_94670308_04631812_31953035_70655036_06106877_89376508_08469879,\n  256'h52294239_94815873_05391186_79008623_37774288_68873584_92037217_41279305"),
+]
+
+class TestBlockify:
+    """Test the method common.blockify()"""
+
+    @staticmethod
+    @pytest.mark.parametrize(("s", "limit", "expected"), blockify_checks)
+    def test_blockify(
+        s: str,
+        limit: int,
+        expected: str,
+    ) -> None:
+        assert_that(common.blockify(s, limit), equal_to(expected))
+
+
